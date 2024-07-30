2639c32e6fd36f43d652d1ca8da17ddef5731b1f
Nimsara Liyanage <tharindanimsara457@gmail.com>
2024-07-30 18:03:24 +0530

    Final Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-
+// src/App.js
 import React from 'react';
-import './App.css';
 import Weather from './Components/weather';
+import GlobalStyle from './GlobalStyle';
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <main>
-        <Weather />
-      </main>
-    </div>
+    <>
+      <GlobalStyle />
+      <Weather />
+    </>
   );
-};
+}
 
 export default App;
