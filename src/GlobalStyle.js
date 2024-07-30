12c6e7c545e0d7b37827ac5b31240c1d604727de
Nimsara Liyanage <tharindanimsara457@gmail.com>
2024-07-30 18:04:11 +0530

    Created Style.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,18 @@
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Arial', sans-serif;
+    background-color: #282c34;
+    color: white;
+  }
+`;
+
+export default GlobalStyle;
